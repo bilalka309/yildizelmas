f2ad5fa096f2ee4d947ca02a35dcc2578260529d
bilal karahan <bilalkarahan309@gmail.com>
2022-09-21 14:22:38 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inp=new Scanner(System.in);
+        System.out.println("N sayısını giriniz:");
+        int x= inp.nextInt();
+        for(int i=1;i<=x;i++){
+            for(int k=1;k<=(x-i);k++){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=(2*i)-1;c++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for(int i=1;i<=x;i++){
+            for(int k=1;k<=i;k++){
+                System.out.print(" ");
+            }
+            for(int d=1 ;d<=(2*(x-i))-1 ; d++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
     }
 }
